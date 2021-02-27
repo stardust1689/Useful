11b5d0294ce90f269593ada6c457c96a070c9d59
Christopher Peters <53628686+stardust1689@users.noreply.github.com>
2021-02-27 10:46:30 -0900

    Add files via upload

--- Log Maker/log_maker_html.py
+++ Log Maker/log_maker_html.py
@@ -8,12 +8,13 @@ def make_logs(month, year):
     p = engine()
     with open(this_month + '_' + str(year) + '.html', 'w') as f:
         f.write(
-            f"<p><strong>CP's Logs ({this_month} {year})\n______________________________________________________________________________\n</strong></p>")
+            f"<p><strong>CP's Logs ({this_month} {year})<br>\n____________________________________________________________________________\n</strong><br><br>")
         for day in range(1, number_of_days + 1):
             weekday = date(year, month, day).strftime('%A')
             f.write(
-                f'<p><strong>{weekday}, {this_month} {p.ordinal(day)}, {year}<br><br></strong></p>')
-            f.write('<p><strong>END LOG\n')
-            f.write('______________________________________________________________________________\n</strong></p>')
+                f'<strong>{weekday}, {this_month} {p.ordinal(day)}, {year}</strong><br><br> <br>')
+            f.write('<strong>END LOG</strong><br>')
+            f.write('____________________________________________________________________________<br><br>')
+        f.write('</p>')
 
-make_logs(1, 2021)
+make_logs(3, 2021)
