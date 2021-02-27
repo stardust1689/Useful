83ff4129a0f91da1df57c3853bd3f1379fffe75d
Christopher Peters <53628686+stardust1689@users.noreply.github.com>
2021-02-27 10:48:06 -0900

    Update log_maker_html.py

--- Log Maker/log_maker_html.py
+++ Log Maker/log_maker_html.py
@@ -12,7 +12,7 @@ def make_logs(month, year):
         for day in range(1, number_of_days + 1):
             weekday = date(year, month, day).strftime('%A')
             f.write(
-                f'<strong>{weekday}, {this_month} {p.ordinal(day)}, {year}</strong><br><br> <br>')
+                f'<strong>{weekday}, {this_month} {p.ordinal(day)}, {year}</strong><br><br><br>')
             f.write('<strong>END LOG</strong><br>')
             f.write('____________________________________________________________________________<br><br>')
         f.write('</p>')
